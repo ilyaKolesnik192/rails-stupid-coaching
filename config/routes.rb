7fea95060f125b4bcde761c0f4b14dcc8916fc14
ilya <iliya92kolesnik@gmail.com>
2023-11-13 15:05:21 +0100

    rails-stupid-coaching

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  root to: "questions#home"
+  get "ask", to: "questions#ask"
+  get "answer", to: "questions#answer"
+ # VERB "path/url", to: "controllername#action"
   # root "posts#index"
 end
